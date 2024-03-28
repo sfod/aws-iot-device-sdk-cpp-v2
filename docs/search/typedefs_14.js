5a6f4b19c7284d8d1cb36ba7cfee90bf62a5b442
Igor Abdrakhimov <igorabd@amazon.com>
2024-03-28 10:21:26 -0700

    Add docs

---
+++ docs/search/typedefs_14.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['value_5ftype_0',['value_type',['../class_aws_1_1_crt_1_1basic__string__view.html#adff2d2e1dd330c567f84e5905d42e6e1',1,'Aws::Crt::basic_string_view']]],
+  ['vector_1',['Vector',['../namespace_aws_1_1_crt.html#a736e4f0f4bd6afaf9cc185314531af9f',1,'Aws::Crt']]]
+];
---
+++ docs/search/typedefs_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['istlsalpnsupportedcallback_0',['IsTlsAlpnSupportedCallback',['../namespace_aws_1_1_crt_1_1_io.html#a0de6144df0c69d49821917bffdf62786',1,'Aws::Crt::Io']]],
+  ['istream_1',['IStream',['../namespace_aws_1_1_crt_1_1_io.html#ad22aabc68a449d16148f4abd250280f5',1,'Aws::Crt::Io']]],
+  ['iterator_2',['iterator',['../class_aws_1_1_crt_1_1basic__string__view.html#ab9e3918a1235dc5b5f8019bb4fc04fbb',1,'Aws::Crt::basic_string_view']]]
+];
